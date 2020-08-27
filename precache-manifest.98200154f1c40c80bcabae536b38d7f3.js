4845c376ea9ac8bfa6153ec110fd794ac76c823a
Mehdi Raza Kazi <mehdi.raza@koderlabs.com>
2020-08-27 19:34:36 +0500

    Updates

---
+++ precache-manifest.98200154f1c40c80bcabae536b38d7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7f64d3a644da62a885c300422efa72",
+    "url": "/tictacttoe/index.html"
+  },
+  {
+    "revision": "39401d5e769c758cac9d",
+    "url": "/tictacttoe/static/css/main.3ae50c87.chunk.css"
+  },
+  {
+    "revision": "5b52284c829884208976",
+    "url": "/tictacttoe/static/js/2.840a5a18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictacttoe/static/js/2.840a5a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39401d5e769c758cac9d",
+    "url": "/tictacttoe/static/js/main.66fcc3a3.chunk.js"
+  },
+  {
+    "revision": "7246e2956f83efa3299f",
+    "url": "/tictacttoe/static/js/runtime-main.df2c3502.js"
+  }
+]);
